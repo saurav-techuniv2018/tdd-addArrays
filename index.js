4670f4eb36f1a641acdfe282b19fb8705d26fac7
Saurav Sahu <sauravsahu.12@live.com>
2018-01-13 11:28:02 +0530

    Test passes for arrays of equal length containing only numbers

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function addArrays(firstArray, secondArray) {
+    let sumArray = [];
+    for (let i = 0; i < firstArray.length; ++i) {
+        sumArray[i] = firstArray[i] + secondArray[i];
+    }
+    return sumArray;
+}
+
 //TEST addArrays
 console.log('Test for arrays of equal length containing only numbers:',
 compareArrays(addArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]), [7, 9, 11, 13, 15]));
