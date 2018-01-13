44a6d9d49e77f5e0940f766d5010f6f728880c57
Saurav Sahu <sauravsahu.12@live.com>
2018-01-13 11:30:30 +0530

    Test failed - arrays of different length

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function addArrays(firstArray, secondArray) {
 //TEST addArrays
 console.log('Test for arrays of equal length containing only numbers:',
 compareArrays(addArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]), [7, 9, 11, 13, 15]));
+console.log('Test for arrays of different length containing numbers:',
+(addArrays([3, 4, 5], [6, 7, 8, 9, 10]) === null));
 
 function compareArrays(firstArray, secondArray) {
     if (firstArray.length !== secondArray.length) { return false; }
